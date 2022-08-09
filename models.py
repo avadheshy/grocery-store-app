4371f491597fadf66783f18675f250630136b190
avadheshy <avadheshy2015@gmail.com>
2022-08-09 15:17:25 +0530

    validation added

--- models.py
+++ models.py
@@ -1,5 +1,4 @@
-from distutils.command.upload import upload
-from pydoc import describe
+from typing import Union
 import uuid
 from typing import Optional
 from datetime import datetime
@@ -9,18 +8,18 @@ from fastapi import File, UploadFile
 
 
 class Product(BaseModel):
-    product_id: int = Field(...)
-    slug: str = Field(...)
-    title: str = Field(...)
-    mrp: int = Field(...)
-    discount_id: int = Field(...)
-    description: str = Field(...)
-    rating: int = Field(...)
-    tag: str = Field(...)
-    category: str = Field(...)
-    sub_category: str = Field(...)
-    brand: str = Field(...)
-    score: int = Field(...)
+    product_id: int = Field(gte=0 ,title="product id must be greater than or equal to zero")
+    slug: str = Field(default=None, max_length=100)
+    title: str= Field(default=None, max_length=100)
+    mrp: int = Field(gt=0,title="mrp must be grater than zero")
+    discount_id: int = Field(gte=0,title="discount must be grater or equal than zero")
+    description: str = Field(default=None,max_length=100)
+    rating: int = Field(gte=0,title="rating must be less than or equal to 5")
+    tag:str = Field(default=None,max_length=100)
+    category: str= Field(default=None,max_length=100)
+    sub_category: str = Field(default=None,max_length=100)
+    brand: str = Field(default=None,max_length=100)
+    score: int = Field(gte=0,lte=20,title="score must be range of 0 to 20")
     #img: bytes  =File(...)
     #rating: int= Field(...)
     # date_created:datetime=Field()
@@ -35,21 +34,23 @@ class ProductUpdate(BaseModel):
     description: Optional[str]
     category: Optional[str]
     mrp: Optional[int]
+    is_active:Optional[bool]
 
 
 # class Image(BaseModel):
 #     url: str
 #     name: str
 
-class Varients(BaseModel):
-    id: int = Field(...)
-    product_id: int = Field(...)
-    color: str = Field(...)
-    size: int = Field(...)
-    quantity: int = Field(...)
-    #child_image:Union[Image, None] = None
-    sku: int = Field(...)
+class Variants(BaseModel):
+    id: int = Field(gte=0 )
+    product_id: int = Field(gte=0)
+    color: str = Field(default=None, title="The description of the item", max_length=100)
+    size: int = Field(gte=0,title="size must be grater or equal than zero")
+    quantity: int = Field(gte=0,title="quantity must be grater or equal than zero")
+    sku: int = Field(gte=0,title="sku must be grater or equal than zero")
     is_available: bool = Field(...)
+    class Config:
+    	orm_mode=True
 
 
 class sku(BaseModel):
--- routes.py
+++ routes.py
@@ -1,13 +1,12 @@
 
-from fastapi import APIRouter, Body, Request, Response, HTTPException, status, Query
+from fastapi import APIRouter, Body, Request, Response, HTTPException, status, Query,FastAPI
 from fastapi.encoders import jsonable_encoder
 from typing import List
-from fastapi_pagination import Page
+from fastapi_pagination import Page,LimitOffsetPage,paginate,add_pagination
 from typing import Optional
-from models import Product, ProductUpdate, Varients
+from models import Product, ProductUpdate, Variants
 from fastapi import File, UploadFile
 router = APIRouter()
-
 
 @router.post("/", response_description="Create a new product", status_code=status.HTTP_201_CREATED, response_model=Product)
 def create_prod(request: Request, item: Product = Body(...)):
@@ -19,9 +18,19 @@ def create_prod(request: Request, item: Product = Body(...)):
     )
     if created_product is not None:
         return created_product
+@router.post("/variant", response_description="Create a new product", status_code=status.HTTP_201_CREATED, response_model=Variants)
+def add_variant(request: Request, item: Variants = Body(...)):
+    item = jsonable_encoder(item)
+
+    new_product = request.app.database["varients"].insert_one(item)
+    created_product = request.app.database["varients"].find_one(
+        {"_id": new_product.inserted_id}
+    )
+    if created_product is not None:
+        return created_product
 
 
-@router.get("/", response_description="List all products", status_code=status.HTTP_200_OK, response_model=List[Product])
+@router.get("/", response_description="List all products", status_code=status.HTTP_200_OK, response_model=list[Product])
 def list_products(request: Request, Categotry: list[str] = Query(None),
                   Brand: list[str] = Query(None), min_range: Optional[int] = None,
                   max_range: Optional[int] = None, Sort: list[str] = Query(None)):
@@ -44,11 +53,11 @@ def list_products(request: Request, Categotry: list[str] = Query(None),
                 brand_list.append({'brand': i})
         if len(category_list) > 0:
             ans.append({"$or": brand_list})
-    print(ans)
+
     if min_range != None and max_range != None:
         ans.append(
             {"$and": [{'mrp': {'$gt': min_range}}, {'mrp': {'$lt': max_range}}]})
-    print(ans)
+    ans.append({'is_active':True})
     if Sort != None:
         if len(ans) >= 1:
             prod = list(request.app.database["trial_prod"].find(
@@ -67,7 +76,7 @@ def list_products(request: Request, Categotry: list[str] = Query(None),
             return prod
 
 
-@router.get("/search", response_description="List all products", response_model=List[Product])
+@router.get("/search", response_description="List all products", status_code=status.HTTP_200_OK,response_model=List[Product])
 def search(request: Request, search_char: str):
     prod = list(request.app.database["trial_prod"].find({"$or": [{'title': {'$regex': search_char, '$options': "i"}},
             {'brand': {
@@ -76,15 +85,21 @@ def search(request: Request, search_char: str):
     return prod
 
 
-@router.get("/{id}", response_description="Get a single product by product_id", response_model=Product)
+@router.get("/{id}", response_description="Get a single product by product_id",status_code=status.HTTP_200_OK, response_model=Product)
 def find_product(id: int, request: Request):
-    if (prod := request.app.database["trial_prod"].find_one({"product_id": id})) is not None:
+    if (prod := request.app.database["trial_prod"].find_one({"product_id": id,'is_active':True})) is not None:
         return prod
     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                         detail=f"Product with ID {id} not found")
 
+# #@router.get("/products",response_model=Page[Product])
+# @router.get("/products/limit-offset",response_model=LimitOffsetPage[Product])
+# def find(request: Request):
+#     prod=paginate(list(request.app.database["trial_prod"].find({})))
+#     return prod
 
-@router.put("/{id}", response_description="Update a product", response_model=Product)
+
+@router.put("/{id}", response_description="Update a product", status_code=status.HTTP_205_RESET_CONTENT, response_model=Product)
 def update_product(id: int, request: Request, prod: ProductUpdate = Body(...)):
     item = {k: v for k, v in prod.dict().items() if v is not None}
 
@@ -92,6 +107,9 @@ def update_product(id: int, request: Request, prod: ProductUpdate = Body(...)):
         update_result = request.app.database["trial_prod"].update_one(
             {"product_id": id}, {"$set": item}
         )
+        if update_result.modified_count == 0:
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"Product with ID {id} not found")
 
     if (
         existing_product := request.app.database["trial_prod"].find_one({"product_id": id})
@@ -102,7 +120,7 @@ def update_product(id: int, request: Request, prod: ProductUpdate = Body(...)):
                         detail=f"prodoct with ID {id} not found")
 
 
-@router.put("/delete/{id}", response_description="Soft delete a product", response_model=Product)
+@router.put("/delete/{id}", response_description="Soft delete a product",status_code=status.HTTP_202_ACCEPTED, response_model=Product)
 def delete_product(id: int, request: Request):
 
     update_result = request.app.database["trial_prod"].update_one(
